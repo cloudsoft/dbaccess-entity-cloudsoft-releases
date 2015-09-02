6a0da1bf60573471f78523dd64efc8d3a6a0ea2a
Martin Harris <github@nakomis.com>
2015-09-02 15:23:17 +0100

    Fixes GRANT_PERMISSIONS statement for postgresql

--- src/main/java/io/cloudsoft/dbaccess/client/PostgresAccessClient.java
+++ src/main/java/io/cloudsoft/dbaccess/client/PostgresAccessClient.java
@@ -2,18 +2,12 @@ package io.cloudsoft.dbaccess.client;
 
 import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import com.google.common.collect.ImmutableList;
 
 public class PostgresAccessClient extends AbstractDatabaseAccessClient {
 
-    private static final Logger LOG = LoggerFactory.getLogger(PostgresAccessClient.class);
-
     private static final String CREATE_USER = "CREATE USER %s WITH ENCRYPTED PASSWORD '%s'";
-    private static final String GRANT_PERMISSIONS = "ALTER DEFAULT PRIVILEGES IN SCHEMA public\n" +
-            "GRANT SELECT ON TABLES TO %s;";
+    private static final String GRANT_PERMISSIONS = "GRANT SELECT ON ALL TABLES IN SCHEMA public TO %s;";
 
     public PostgresAccessClient(String endpoint, String adminUsername, String adminPassword, String database) {
         super(endpoint, adminUsername, adminPassword, database);
